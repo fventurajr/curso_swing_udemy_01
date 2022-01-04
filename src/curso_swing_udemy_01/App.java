6161a52d53c94a57c8a78bbbb6cd77732b774139
Fernando Ventura Jr <fventurajr@gmail.com>
2022-01-03 22:35:22 -0300

    Using a thread with SwingUtilities.invokeLater() method

--- src/curso_swing_udemy_01/App.java
+++ src/curso_swing_udemy_01/App.java
@@ -1,14 +1,24 @@
 package curso_swing_udemy_01;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class App {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Hello World");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 400);
-		frame.setVisible(true);
+		
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				JFrame frame = new JFrame("Hello World");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setSize(400, 400);
+				frame.setVisible(true);				
+			}
+			
+		});
+
 	}
 
 }
