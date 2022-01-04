e259051fd48adff1350be6ce520e0148fbe39ce9
Fernando Ventura Jr <fventurajr@gmail.com>
2022-01-03 22:28:35 -0300

    JFrame with a Hello World title

---
+++ src/curso_swing_udemy_01/App.java
@@ -0,0 +1,14 @@
+package curso_swing_udemy_01;
+
+import javax.swing.JFrame;
+
+public class App {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Hello World");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+	}
+
+}
